20ff16ba2ac7b8e47a421b13899d80d4fbb7bb69
sar1yeva <yaxsisariyeva55@gmail.com>
2025-11-22 15:05:17 +0000

    Python

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a square class with size validation."""
+"""Defines a square class with size validation and area computation."""
 
 
 class Square:
@@ -17,8 +17,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
     def area(self):
         """Return the current square area."""
-
         return self.__size * self.__size
